dee7720838c2e2b3fff0e2892befcf957bd36e9d
neil.garner <neil.garner@autotrader.co.uk>
2017-12-20 10:39:28 +0000

    Removal of clean up webpack module

--- web/webpack/webpack.prod.config.js
+++ web/webpack/webpack.prod.config.js
@@ -2,7 +2,6 @@ const webpack = require('webpack');
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const merge = require('webpack-merge');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const baseConfig = require('./webpack.common.config.js');
 
@@ -12,10 +11,6 @@ module.exports = merge(baseConfig, {
         filename: '[name].js'
     },
     plugins: [
-        // Clear out `build` directory between builds
-        new CleanWebpackPlugin(['build'], {
-            root: process.cwd()
-        }),
         // Extract imported CSS into own file
         new ExtractTextPlugin('[name].bundle.[chunkhash].css'),
         // Minify JS
